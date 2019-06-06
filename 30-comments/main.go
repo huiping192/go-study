4e64ba7b1e90569b24820ed6423f3c6c36056132
huiping.guo <huiping.guo@dena.jp>
2019-06-06 23:32:15 +0900

    add comments sample

---
+++ 30-comments/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+// print hell
+// return nothing
+func a() {
+	fmt.Println("hello") // print hello
+}
+
+/*
+ * this is super comment
+ */
+func b() {
+
+}
